4f6d8dfe60e33574d6457d701e4cfa30716e8cd7
Simone Westphal <hello@swestphal.io>
2021-05-21 20:00:43 +0200

    feature: Login user

--- admin/users/views.py
+++ admin/users/views.py
@@ -7,13 +7,29 @@ from .serializers import UserSerializer
 @api_view(['POST'])
 def register(request):
     data = request.data
-    print(data)
     if data['password'] != data['password_confirm']:
         raise exceptions.APIException('Passwords do not match!')
     serializer = UserSerializer(data=data)
     serializer.is_valid(raise_exception=True)
     serializer.save()
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def login(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+
+    user = User.objects.filter(email=email).first()
+
+    if user is None:
+        raise exceptions.AuthenticationFailed('User not found')
+
+    if not user.check_password(password):
+        raise exceptions.AuthenticationFailed('Incorrect password')
+
+    return Response('success')
+
 
 @api_view(['GET'])
 def users(request):
